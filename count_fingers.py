b921b7486d7370be9c2730d21089cbe604950244
bjsfsmx-0544 <94998868+bjsfsmx-0544@users.noreply.github.com>
2022-03-02 22:32:55 -0600

    Traducción 1.0
    Modificación de elementos en pantalla. Actualización de README.md

--- count_fingers.py
+++ count_fingers.py
@@ -5,11 +5,10 @@ cap = cv2.VideoCapture(0)
 while True:
     success, image = cap.read()
 
-    cv2.imshow("Media Controller", image)
+    cv2.imshow("Controlador de medios", image)
 
     key = cv2.waitKey(1)
     if key == 32:
         break
 
 cv2.destroyAllWindows()
-
